408cf470b0aa672fecc6187c7e15166b862ed616
PurpleNinjaLink <jawhittom@gmail.com>
2018-02-20 14:42:02 -0600

    Pickups Started
    Started implementing pickups using scripts from 3D project last semester. Need to add a basic inventory menu so things don't freak out

---
+++ Assets/Scripts/InventoryMenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenuItem : MonoBehaviour 
+{
+    private InventoryMenu inventoryMenu;
+    private AudioSource audioSource;
+
+    public InventoryObject InventoryObjectRepresented { get; set; }
+
+    void Start () 
+	{
+        inventoryMenu = FindObjectOfType<InventoryMenu>();
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    public void OnValueChanged()
+    {
+        // Update the description area text!
+        inventoryMenu.UpdateDescriptionAreaText(InventoryObjectRepresented.DescriptionText);
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : MonoBehaviour, IActivatable
+{
+    [SerializeField]
+    private string nameText;
+
+    [SerializeField]
+    private string descriptionText;
+
+    [SerializeField]
+    private Light light;
+
+    private AudioSource audioSource;
+    private MeshRenderer meshRenderer;
+    private MeshRenderer[] childRenderers;
+    private InventoryMenu inventoryMenu;
+    private Collider collider;
+
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public string DescriptionText { get { return descriptionText; } }
+
+    private void Start()
+    {
+        inventoryMenu = FindObjectOfType<InventoryMenu>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+        childRenderers = GetComponentsInChildren<MeshRenderer>();
+        descriptionText = descriptionText.Replace("\\n", "\n");
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void DoActivate()
+    {
+        inventoryMenu.PlayerInventory.Add(this);
+        audioSource.Play();
+        // Doing this rather than destroy because our Inventory menu still needs
+        // to know about this object even though it has been collected and 
+        // removed from the 3D world.
+        // Also, if you wanted to add sound effects here,
+        // and we destroy before the sfx are done, it will not sound correct.
+        // Just like how coin worked in our 2D project!
+        if (light != null)
+            light.enabled = false;
+        meshRenderer.enabled = false;
+        if(childRenderers != null)
+            foreach (MeshRenderer r in childRenderers)
+                r.enabled = false;
+        collider.enabled = false;        
+    }
+}
